d56cbea4a783f2d03ed8ab95174eed5c8ecf7b01
Jake Houston <jakechouston@gmail.com>
2021-03-29 19:11:51 -0400

    commit tree 400a15976c1aa553159425071ef9fb473e0376a7

--- index.js
+++ index.js
@@ -1,1 +1,55 @@
-// Add your code here
+// // Add your code here
+// const formData = {
+//     dogName: "Byron" ,
+//     dogBreed: "Poodle"
+// };
+
+// const configurationObject = {
+//     // method: "POST" ,
+//     headers: {
+//         "Content-Type": "application/json" ,
+//         "Accept": "application/json"
+//     } ,
+//     body: JSON.stringify(formData)
+// };
+
+// fetch("http://localhost:3000/dogs" , configurationObject)
+//     .then(function(response) {
+//         return response.json();
+//     })
+//     .then(function(object) {
+//         console.log(object);
+//     })
+//     .catch(function(error) {
+//         alert("Bad things! Ragnarők!");
+//         console.log(error.message);
+//     });
+
+const formData = {
+    name: "Steve" ,
+    email: "steve@steve.com"
+}
+
+function submitData() {
+    return fetch("http://localhost:3000/users" , {
+        method: "POST" ,
+        headers: {
+            "Content-Type": "application/json" ,
+            "Accept": "application/json"
+        } ,
+        body: JSON.stringify(formData)
+    })
+    .then(function(response) {
+        return response.json();
+    })
+    .then(function(object) {
+        const newDiv = document.createElement("div");
+        newDiv.innerHTML = object.id;
+        document.body.appendChild(newDiv);
+    })
+    .catch(function(error) {
+        const errorDiv = document.createElement("div");
+        errorDiv.innerHTML = error.message;
+        document.body.appendChild(errorDiv);
+    });
+}
